dc30b009cd2bcd6232d9a3b5057fcc0cb86cd0ac
ferrantejake <jake.ferrante@hotmail.com>
2017-11-09 18:53:54 -0500

    Clean up write_chunks and modify to use model data

--- io_examples/file_functs.py
+++ io_examples/file_functs.py
@@ -18,8 +18,9 @@ def write_files():
         f = open(file, 'w')
 
         for x in range(100000000000000000, 100000000000010000):
-            f.write('%d, %d,%d,%d,%d,%d,%d,%d\n' %
-                    (x, x + 1, x + 2, x + 3, x + 4, x + 5, x + 6, x + 7))
+            r = x + 0.0000000000000001
+            f.write('%d,%d,%d,%d,%d,%d,%d,%d\n' %
+                    (r, r + 1, r + 2, r + 3, r + 4, r + 5, r + 6, r + 7))
 
 
 def write_files_with():
@@ -27,9 +28,10 @@ def write_files_with():
         file = '%s%s%d.txt' % (directory, file_name, fileiteration)
 
         with open(file, 'w') as f:
-            for x in range(100000000000000000, 100000000000010000):
+            for x in range(100000000000000001, 100000000000010000):
+                r = x + 0.0000000000000001
                 f.write('%d, %d,%d,%d,%d,%d,%d,%d\n' %
-                        (x, x + 1, x + 2, x + 3, x + 4, x + 5, x + 6, x + 7))
+                        (r, r + 1, r + 2, r + 3, r + 4, r + 5, r + 6, r + 7))
 
 
 def write_chunks(chunk_size):
@@ -39,16 +41,21 @@ def write_chunks(chunk_size):
         chunk_size = 100
 
     for fileiteration in range(num_files):
-        file = '%s%s%d.txt' % (directory, file_name, fileiteration)
+        path = '%s%s%d.txt' % (directory, file_name, fileiteration)
 
-        with open(file, 'w') as f:
-            buf = [num_data_points][chunk_size]
-            for x in range(100000000000000000, 100000000000010000):
-                if ((x + 1) % chunk_size) == 0:
+        with open(path, 'w') as f:
+            buf = []
+            buf.append(['Time (s)', 'Channel 0', 'Channel 1', 'Channel 2',
+                        'Channel 3', 'Channel 4', 'Channel 5', 'Channel 6', 'Channel 7'])
+
+            for x in range(100000000000000001, 100000000000010000):
+                r = x + 0.0000000000000001
+                buf.append([r, r + 1, r + 2, r + 3,
+                            r + 4, r + 5, r + 6, r + 7])
+                if (x % chunk_size) == 0:
+                    # print('writing data for x=%s' % (x - 100000000000000000))
                     f.write(join_data_points(buf))
-
-# Write to buffer then convert to string once buffer is full
-# Skip buffer and concatenate to string and write string
+                    buf = []
 
 
 def join_array_to_string(array, delimiter):
@@ -56,7 +63,11 @@ def join_array_to_string(array, delimiter):
 
 
 def join_data_points(data_points):
-    for row_index in len(data_points):
+    for row_index in range(0, len(data_points) - 1):
+        # print('row_index: %d data_points[%d]: %s' % (
+            # row_index, row_index, str(data_points[row_index + 1])))
         data_points[row_index] = join_array_to_string(
-            data_points[row_index], ',')
-    return join_array_to_string(data_points, ';')
+            data_points[row_index],
+            ','
+        )
+    return join_array_to_string(data_points, ',\n')

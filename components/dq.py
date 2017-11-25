f6b93742c6cd1468f85543c5b8e1e2183d2253ec
ferrantejake <jake.ferrante@hotmail.com>
2017-11-24 21:52:08 -0500

    Ignore data file Add pylint Add resetBuffers to fileio Fix join data points in fileio Remove main.py Clean up multi.py Comment/remove print statements & comments

--- components/dq.py
+++ components/dq.py
@@ -1,24 +1,4 @@
-# import docstring
-
-
-# class DQ:
-#     """Data Queue"""
-
-#     isBeingAccessed = False
-
-#     def __init__(self, chunkSize):
-#         self.chunk_size = chunkSize
-#         self.queue = []
-
-#     def push(data):
-#         # Expects some data as array or item
-
-#     def pop():
-
-#     def pop(n):
-
-
-from multiprocessing import Pipe, Process
+from multiprocessing import Pipe
 import sys
 
 MAX_POLLING_TIMEOUT = sys.maxsize
@@ -42,13 +22,8 @@ class Dq:
         If data is an array, push data points individually.
         """
 
-        if isinstance(data, list) is True:
-            print('sending data point[s]')
-            for i, data_point in enumerate(data):
-                self._producer.send(data_point)
-        else:
-            print('sending data point')
-            self._producer.send(data)
+        # print('sending data point')
+        self._producer.send(data)
 
     def poll(self, timeout_in_seconds):
         """
@@ -60,10 +35,10 @@ class Dq:
         while self.shouldpoll is True:
             if self._consumer.poll(timeout_in_seconds) is True:
                 data_point = self._consumer.recv()
-                print('data ' + data_point)
+                # print('data ' + data_point)
                 self.ondata(data_point)
-            else:
-                print('no new data')
+            # else:
+            #     print('no new data')
 
     def enablepolling(self):
         """Set semaphore positive for polling. Start polling pipe for data."""
--- components/fileio.py
+++ components/fileio.py
@@ -3,30 +3,48 @@ class ChunkedFileIOQ:
     buf = []
     overflow = []
 
-    def __init__(self, file_path, chunk_size):
+    def __init__(self, file_path, chunk_size, header_values):
         self.chunk_size = chunk_size
         self.file_path = file_path
         self.buf_is_locked = False
+        self.header_values = header_values
+        self.resetBuffersWithHeaders()
 
     def _write(self):
         """Force a write to disk and flush buffer"""
         with open(self.file_path, 'w') as file_out:
-            file_out.write(join_data_points(self.buf))
-            self._flush()
+            file_out.write(joinDataPoints(self.buf))
 
     def push(self, data_point):
         """Push data to data buffer"""
+        # print('buffer length [%d], chunk size [%d]' %
+        #   (len(self.buf), self.chunk_size))
         if self.buf_is_locked is True:
+            # print('buffer is locked')
             self.overflow.append(data_point)
             return
 
+        # print('buffer not locked')
         self.buf.append(data_point)
-        if len(self.buf) is self.chunk_size:
-            self.buf_is_locked = True
+        if len(self.buf) == self.chunk_size:
             self._write()
-            self.buf = self.overflow
-            self.overflow = []
-            self.buf_is_locked = False
+            self.resetBuffers()
+        else:
+            print('%d /= %d' % (len(self.buf), self.chunk_size))
+
+    def resetBuffersWithHeaders(self):
+        self.buf_is_locked = True
+        self.buf = self.header_values
+        for overflowval in self.overflow:
+            self.buf.append(overflowval)
+        self.overflow = []
+        self.buf_is_locked = False
+
+    def resetBuffers(self):
+        self.buf_is_locked = True
+        self.buf = self.overflow
+        self.overflow = []
+        self.buf_is_locked = False
 
     def _flush(self):
         """Flush data buffer"""
@@ -41,19 +59,20 @@ class ChunkedFileIOQ:
         # write then callback
 
 
-def join_array_to_string(array, delimiter):
+def joinArrayToString(array, delimiter):
     """Join array to string"""
-    return delimiter.join(['{val}' for val in array])
+    return delimiter.join([str(val) for val in array])
 
 
-def join_data_points(data_points):
+def joinDataPoints(data_points):
     """
-    Join items in an array  into a string on a comma delimiter.
+    Join items in an array into a string on a comma delimiter.
     End new string with new line character
     """
+    print(data_points)
     for row_index in range(0, len(data_points) - 1):
-        data_points[row_index] = join_array_to_string(
+        data_points[row_index] = joinArrayToString(
             data_points[row_index],
             ','
         )
-    return join_array_to_string(data_points, ',\n')
+    return joinArrayToString(data_points, ',\n')
